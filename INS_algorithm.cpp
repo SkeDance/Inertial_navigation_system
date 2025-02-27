051b4f719b701900b6b30e4941288320c5e8be01
SkeDance <qoupskd@gmail.com>
2025-02-27 21:30:30 +0300

    update reworked algorithm

--- INS_algorithm.cpp
+++ INS_algorithm.cpp
@@ -11,12 +11,12 @@ bool flag = 0;
 
 const float g = 9.81;
 //shirota & dolgota / f % a
-float latitude_0 = 55.44;
-float longitude_0 = 37.36;
+float latitude_0;
+float longitude_0;
 
 float R_Earth = 6.371 * pow(10, 6);
-float R_latitude = (R_Earth * (1 - pow(M_E, 2))) / pow(1 - pow(M_E, 2) * pow(sin(latitude_0), 2), 3 / 2);
-float  R_longitude = (R_Earth * (1 - pow(M_E, 2))) / pow(1 - pow(M_E, 2) * pow(sin(longitude_0), 2), 1 / 2);
+float R_latitude;  
+float  R_longitude;   
 
 float C_0;
 
@@ -40,8 +40,6 @@ float X, Y, Z;
 float matrix_LL[3][3]; //матрица для выставки на первом такте 
 float Acc_matrix_BL[3][1]; //матрица с показаниями акселерометров
 float Gyro_matrix_BL[3][1]; //матрица с покзааниями ДУСов
-float Acc_matrix_ENUp[3][1] = {0, 0, 0}; //матрица ускорений в системе ENUp
-float NEW_LL_MATRIX[3][3]; // новая матрица, которая используется для перевода в систему ENUp на последующих тактах работы алгоритма
 float matrix_W_B[3][3]; //3x3 матрица показаний ДУСов
 float matrix_W_LL[3][3]; //матрица посчитанных угловых скоростей из показаний акслерометра
 
@@ -60,13 +58,13 @@ float matrix(int i, int j){
 
 float matrix_W_DUS(int i, int j){
     matrix_W_B[0][0] = 0;
-    matrix_W_B[0][1] = -wZ;
-    matrix_W_B[0][2] = wY;
-    matrix_W_B[1][0] = wZ;
+    matrix_W_B[0][1] = -Gyro_matrix_BL[2][0];
+    matrix_W_B[0][2] = Gyro_matrix_BL[1][0];
+    matrix_W_B[1][0] = Gyro_matrix_BL[2][0];
     matrix_W_B[1][1] = 0;
-    matrix_W_B[1][2] = -wX;
-    matrix_W_B[2][0] = -wY;
-    matrix_W_B[2][1] = wX;
+    matrix_W_B[1][2] = -Gyro_matrix_BL[0][0];
+    matrix_W_B[2][0] = -Gyro_matrix_BL[1][0];
+    matrix_W_B[2][1] = Gyro_matrix_BL[0][0];
     matrix_W_B[2][2] = 0;
     return matrix_W_B[i][j];
 }
@@ -84,10 +82,11 @@ float matrix_W_ENUp(int i, int j){
     return matrix_W_LL[i][j];
 }
 
-void bodyToLocal(float aX, float aY, float aZ){
+float bodyToLocal(float aX, float aY, float aZ, int i, int j){
     Acc_matrix_BL[0][0] = aX;
     Acc_matrix_BL[1][0] = aY;
     Acc_matrix_BL[2][0] = aZ;
+    float Acc_matrix_ENUp[3][1] = {0, 0, 0}; //матрица ускорений в системе ENUp
     for(int i = 0, j = 0, k = 0; j <= 2; j++){
         while(k <= 2){
             Acc_matrix_ENUp[j][i] += ((matrix(j, k) * Acc_matrix_BL[k][i]));
@@ -95,6 +94,7 @@ void bodyToLocal(float aX, float aY, float aZ){
         }
         k = 0;
     }
+    return Acc_matrix_ENUp[j][i];
 }
 
 float getX(float accel){
@@ -119,7 +119,8 @@ float getX(float accel){
     return Y;
 }
 
-void Poisson(){
+float Poisson(int i, int j){
+    float NEW_LL_MATRIX[3][3]; // новая матрица, которая используется для перевода в систему ENUp на последующих тактах работы алгоритма
     for(int i = 0, j = 0, k = 0; i <= 2; i++){
       for(; j <= 2; j++){
         for(; k <= 2; k++){
@@ -129,10 +130,12 @@ void Poisson(){
       }
       j = 0;
     }
+    return NEW_LL_MATRIX[i][j];
 }
 
 int main()
 {
+    int latitude = 0;
     //setlocale(LC_ALL, "ru");
     // Путь к документу
     //string path = "test_data.txt";
@@ -187,6 +190,10 @@ int main()
                 //aZ
                 Acc_matrix_BL[2][0] = stof(token);   
             }
+            if(tokenCount == 13){
+                //shirota
+                latitude_0 = stof(token);
+            }
         }
 
         //cout << numLines << "    " << "omegaX = " << Gyro_matrix_BL[0][0] << "   " << "omegaY = " << Gyro_matrix_BL[1][0] << "   " << "omegaZ = " << Gyro_matrix_BL[2][0] << endl;
@@ -205,6 +212,7 @@ int main()
                 PITCH = PITCH_0;
                 ROLL = ROLL_0;
                 YAW = YAW_0;
+                latitude = latitude_0;
                 cout << PITCH << "    " << ROLL << "    " << YAW << endl;
             }
             else{
@@ -212,32 +220,30 @@ int main()
                 //cout << wE << "    " << wN << "    " << wUp << endl;
                 cout << PITCH << "    " << ROLL << "    " << YAW << endl;
 
-                bodyToLocal(Acc_matrix_BL[0][0], Acc_matrix_BL[1][0], Acc_matrix_BL[2][0]);
+                bodyToLocal(Acc_matrix_BL[0][0], Acc_matrix_BL[1][0], Acc_matrix_BL[2][0], 0, 0);
                 
 
-                X = getX(Acc_matrix_ENUp[0][0]);
-                Y = getY(Acc_matrix_ENUp[1][0]);
+                // X = getX(Acc_matrix_ENUp[0][0]);
+                // Y = getY(Acc_matrix_ENUp[1][0]);
+                //Z = getZ(Acc_matrix_ENUp[2][0]);
 
-                Poisson();
+                R_latitude = (R_Earth * (1 - pow(M_E, 2))) / pow(1 - pow(M_E, 2) * pow(sin(latitude_0), 2), 3 / 2);
+                R_longitude = (R_Earth * (1 - pow(M_E, 2))) / pow(1 - pow(M_E, 2) * pow(sin(latitude_0), 2), 1 / 2);
 
-                C_0 = sqrt(((NEW_LL_MATRIX[2][0] * NEW_LL_MATRIX[2][0]) + (NEW_LL_MATRIX[2][2] * NEW_LL_MATRIX[2][2])));
 
-                PITCH = atan(fmod((NEW_LL_MATRIX[2][1] / C_0), 90.0));//(NEW_LL_MATRIX[2][1] / C_0);
-                ROLL = atan(fmod((NEW_LL_MATRIX[2][0] / NEW_LL_MATRIX[2][2]), 180.0));//(NEW_LL_MATRIX[2][0] / NEW_LL_MATRIX[2][2]);
-                YAW = atan(fmod((NEW_LL_MATRIX[0][1] / NEW_LL_MATRIX[1][1]), 360.0));//(NEW_LL_MATRIX[0][1] / NEW_LL_MATRIX[1][1]);
+                //calculate angular velocity
+                wE = -velocityY / R_latitude;
+                wN = velocityX / R_longitude;
+                wUp = velocityX / R_longitude * tan(latitude_0);
+
+                Poisson(0 ,0);
+
+                C_0 = sqrt(((Poisson(2, 0) * Poisson(2, 0)) + (Poisson(2, 2) * Poisson(2, 2))));
+
+                PITCH = atan(fmod((Poisson(2, 1) / C_0), 90.0));//(NEW_LL_MATRIX[2][1] / C_0);
+                ROLL = atan(fmod((Poisson(2, 0) / Poisson(2, 2)), 180.0));//(NEW_LL_MATRIX[2][0] / NEW_LL_MATRIX[2][2]);
+                YAW = atan(fmod((Poisson(0, 1) / Poisson(1, 1)), 360.0));//(NEW_LL_MATRIX[0][1] / NEW_LL_MATRIX[1][1]);
             }
-        }
-        
-        else{
-            
-            //Z = getZ(Acc_matrix_ENUp[2][0]);
-
-            //calculate angular velocity
-            //wE = -velocityY / R_latitude;
-            //wN = velocityX / R_longitude;
-            //wUp = velocityX / R_longitude * tan(latitude_0);
-
-            
         }
     }
 
